8e1cc8fe775c32835ee3b0fbc6bc4d87a904974f
browner86 <50446300+browner86@users.noreply.github.com>
2021-09-30 11:08:55 -0700

    fix ports

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+const PORT = process.env.PORT || 5000; // So we can run on heroku || (OR) localhost:5000
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -20,5 +20,5 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
-app.listen(5000);
-// app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+// app.listen(5000);
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
