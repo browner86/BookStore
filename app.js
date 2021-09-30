0ba571053451f811081e8602266374dcf671dc65
browner86 <50446300+browner86@users.noreply.github.com>
2021-09-30 10:17:32 -0700

    finish Dynamic Routes module need fix for heroku

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const path = require('path');
-const PORT = process.env.PORT || 5000; // So we can run on heroku || (OR) localhost:5000
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -11,15 +12,13 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const { error } = require('console');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin',adminRoutes );
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
-
 app.listen(5000);
 // app.listen(PORT, () => console.log(`Listening on ${PORT}`));
