685555133ceb626f5016f9cb544d17762cbcf583
browner86 <50446300+browner86@users.noreply.github.com>
2021-10-05 19:51:46 -0700

    add mongo db

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const path = require('path');
-const PORT = process.env.PORT || 5000; // So we can run on heroku || (OR) localhost:5000
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
+const mongoConnect = require('./util/database').mongoConnect;
 
 const app = express();
 
@@ -16,9 +17,21 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  // User.findById(1)
+  //   .then(user => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch(err => console.log(err));
+  next();
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
-// app.listen(5000);
-app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+mongoConnect(() => {
+  app.listen(5000);
+});
